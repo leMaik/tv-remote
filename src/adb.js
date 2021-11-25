6c006daa8ffe7012a09f683154132cd52e1f72bb
Maik Marschner <an@maik.tk>
2021-11-26 00:32:03 +0100

    Fix adb commands.

--- src/adb.js
+++ src/adb.js
@@ -3,6 +3,7 @@ const { default: prepend } = require("prepend-transform");
 
 function runAdb(command) {
   return new Promise((resolve, reject) => {
+    const child = exec(`adb ${command}`);
     let errors = "";
     child.stderr.on("data", (chunk) => (errors += chunk.toString()));
     child.on("exit", (code) => {
